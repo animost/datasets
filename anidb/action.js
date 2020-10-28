4b921bc74c915849d297e2f9bdeaea2919d9e586
blukai <20866892+blukai@users.noreply.github.com>
2020-10-28 03:47:21 +0200

    update action

--- anidb/action.js
+++ anidb/action.js
@@ -1,26 +1,40 @@
-const core = require('@actions/core')
-const got = require('got')
-const zlib = require('zlib')
-const fs = require('fs')
-const stream = require('stream')
-const path = require('path')
-const childProcess = require('child_process')
-const util = require('util');
+const childProcess = require("child_process")
+const core = require("@actions/core")
+const fs = require("fs")
+const https = require("https")
+const path = require("path")
+const stream = require("stream")
+const url = require("url")
+const util = require("util")
+const zlib = require("zlib")
 
-const pipeline = util.promisify(stream.pipeline)
-const exec = util.promisify(childProcess.exec)
+const downloadAnimeTitles = outFile => new Promise((resolve, reject) => {
+  const options = url.parse("https://anidb.net/api/anime-titles.dat.gz")
+  options.headers = { "user-agent": "animost" }
 
-async function main() {
+  const gunzip = zlib.createGunzip()
+  const file = fs.createWriteStream(outFile)
+  const callback = res => {
+    stream.pipeline(res, gunzip, file, err => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  }
+
+  https.get(options, callback)
+})
+
+const main = async () => {
   try {
-    const { GITHUB_WORKSPACE = path.join(__dirname, '..') } = process.env
-    const OUT_FILE = `${GITHUB_WORKSPACE}/anidb/anime-titles.dat`
+    const { GITHUB_WORKSPACE = path.join(__dirname, "..") } = process.env
+    const OUT_FILE = path.join(GITHUB_WORKSPACE, "anidb", "anime-titles.dat")
 
-    await pipeline(
-      got.stream('http://anidb.net/api/anime-titles.dat.gz'),
-      zlib.createGunzip(),
-      fs.createWriteStream(OUT_FILE)
-    )
+    await downloadAnimeTitles(OUT_FILE)
 
+    const exec = util.promisify(childProcess.exec)
     const options = {
       cwd: GITHUB_WORKSPACE,
       listeners: {
@@ -29,19 +43,19 @@ async function main() {
       }
     }
 
-    await exec('git config user.name "GitHub"', options)
-    await exec('git config user.email "noreply@github.com"', options)
+    await exec(`git config user.name "GitHub"`, options)
+    await exec(`git config user.email "noreply@github.com"`, options)
 
     await exec(`git add ${OUT_FILE}`, options)
 
     try {
-      await exec('git commit -m "⬆️ anidb"', options)
+      await exec(`git commit -m "⬆️ anidb"`, options)
     } catch (error) {
-      core.debug('nothing to commit, working tree clean');
-      return;
+      core.debug("nothing to commit")
+      return
     }
 
-    await exec('git push --set-upstream origin HEAD:master', options)
+    await exec("git push --set-upstream origin HEAD:master", options)
   } catch (error) {
     core.setFailed(error.message)
   }
